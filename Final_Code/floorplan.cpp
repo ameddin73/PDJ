9d042af573f24200b0e90da5a33de65e4ca699d2
ameddin73 <ameddin73@gmail.com>
2014-03-15 14:32:17 -0400

    apples

--- Final_Code/floorplan.cpp
+++ Final_Code/floorplan.cpp
@@ -87,8 +87,8 @@ floorplan::floorplan(bool nexus) {
     }
 	//check tile type
 	//internal
-	for (int j = 1; j < HEIGHT - 1; j++) {
-		for (int i = 1; i < WIDTH - 1; i++) {
+	for (int j = 1; j < WIDTH - 1; j++) {
+		for (int i = 1; i < HEIGHT - 1; i++) {
 			if (floorplan_[i][j]){
 				floorplan_[i][j] = tile_single;
 				if (//(floorplan_[i - 1][j - 1] == 0) && //top-left
